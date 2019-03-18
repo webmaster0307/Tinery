5b850fb7db7679879c387dd4f12ece98f1926d5f
Web Master <web.master0307@gmail.com>
2019-03-18 16:24:34 +0100

    secured keys

--- config/keys.js
+++ config/keys.js
@@ -1,17 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://administrator:ubiqum2019@ds159634.mlab.com:59634/mytin?authSource=mytin&w=1",
-  secretOrKey: "secret",
-  // google: {
-  //   clientID:
-  //     "24280000651-bvmpenmvj4p7dila2kni75utafi5sqc4.apps.googleusercontent.com",
-  //   clientSecret: "Xv17NGhV8hN2pPrRemjIrksq"
-  // },
-  googleClientID:
-    "24280000651-bvmpenmvj4p7dila2kni75utafi5sqc4.apps.googleusercontent.com",
-  googleClientSecret: "Xv17NGhV8hN2pPrRemjIrksq",
-  facebook: {
-    AppID: "284220635589707",
-    AppSecret: "e5320cbfe14f823263511bf89b3b2626"
-  }
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
